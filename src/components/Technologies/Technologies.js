cb0080297278016b7902ee7102999c9cf6d5a1b8
Advaith U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2022-01-13 23:42:15 +0530

    [FEAT] : Add Technologies and About me section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,56 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { SiPostman, SiReact, SiJavascript } from 'react-icons/si';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section nopadding id='technology'>
+    <SectionDivider/>
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range a technologies and tools during my journey</SectionText>
+    <List>
+      <ListItem>
+        <picture>
+          <SiJavascript size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Languages</ListTitle>
+          <ListParagraph>
+            Experiece with <br />
+            Python, Java, JavaScript
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <SiReact size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Frameworks</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React JS , Next JS
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <SiPostman size="3rem"/>
+        </picture>
+        <ListContainer>
+          <ListTitle>Tools</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Postman, Figma and Notion
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <SectionDivider colorAlt />
+  </Section>
 );
 
 export default Technologies;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -62,9 +62,8 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: "Started my journey" },
-  { year: 2018, text: "Worked as a freelance developer" },
-  { year: 2019, text: "Founded JavaScript Mastery" },
-  { year: 2020, text: "Shared my projects with the world" },
-  { year: 2021, text: "Started my own platform" },
+  { year: 2018, text: "Started my journey" },
+  { year: 2019, text: "Got selected as Micosoft Student Ambassador" },
+  { year: 2020, text: "Got selected as Google Developer Students Club Lead" },
+  { year: 2021, text: "Joined Figma as the India Regional Lead for Friends of Figma" }, 
 ];
