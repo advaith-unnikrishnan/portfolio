4c16235752eaa698e6cdafdbabc58f7d250d40ec
Advaith U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2022-01-13 22:18:44 +0530

    [FEAT] : Add hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,28 @@
-import React from 'react';
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi there <br />
+        I'm Advaith
+      </SectionTitle>
+
+      <SectionText>
+       Developer | Community Evangelist | Product Enthusiast
+      </SectionText>
+      <Button onClick={()=>window.location="https://peerlist.io/advaith"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
