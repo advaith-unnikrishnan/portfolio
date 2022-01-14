91069ee1f0a55f74987e59c3ce9250e530d5c28b
Advaith U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2022-01-14 09:37:52 +0530

    Add Facicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,50 @@
-import Acomplishments from '../components/Accomplishments/Accomplishments';
-import BgAnimation from '../components/BackgroundAnimation/BackgroundAnimation';
-import Hero from '../components/Hero/Hero';
-import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/Timeline/Timeline';
-import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
-
+import Acomplishments from "../components/Accomplishments/Accomplishments";
+import BgAnimation from "../components/BackgroundAnimation/BackgroundAnimation";
+import Hero from "../components/Hero/Hero";
+import Projects from "../components/Projects/Projects";
+import Technologies from "../components/Technologies/Technologies";
+import Timeline from "../components/Timeline/Timeline";
+import { Layout } from "../layout/Layout";
+import { Section } from "../styles/GlobalComponents";
+import Head from "next/head";
 const Home = () => {
   return (
-    <Layout>
-      <Section grid>
-        <Hero />
-        <BgAnimation />
-      </Section>
-      <Projects />
-      <Technologies />
-      <Timeline />
-      {/* <Acomplishments /> */}
-    </Layout>
+    <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
+        <title>Advaith | Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Layout>
+        <Section grid>
+          <Hero />
+          <BgAnimation />
+        </Section>
+        <Projects />
+        <Technologies />
+        <Timeline />
+        {/* <Acomplishments /> */}
+      </Layout>
+    </>
   );
 };
 
