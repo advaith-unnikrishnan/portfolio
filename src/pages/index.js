ca4628bce7248a6c8cf78e986b9fbf7282f0f898
Advaith U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2022-01-14 08:53:52 +0530

    [FIX]: Typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import BgAnimation from '../components/BackgroundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
+import Timeline from '../components/Timeline/Timeline';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
