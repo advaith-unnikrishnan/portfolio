532daf5bb19f50515540dd6c78e1a2b868e73eb8
Advaith U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2022-01-30 20:56:45 +0530

    Add Meta Info

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,17 +31,40 @@ const Home = () => {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="theme-color" content="#ffffff" />
-        <title>Advaith | Home</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Advaith U is a Web Developer, Community Evangelist and a tech speaker. He has prior experience working as a Google DSC Lead and Microsoft Student Ambassador."
-        />
+
+        {/* Primary Meta Tags */}
         <meta
           name="keywords"
           content="Advaith, Advaith U, Web Development, DSC Lead , Explore ML facilitator, Microsoft Student Ambassador, React Developer, Product Manager, Program Manager, Community evangelist in South India, Community Manager in South India, Tech Speaker"
         />
         <meta name="author" content="Advaith U" />
+        <title>Advaith U | Home</title>
+        <meta name="title" content="Advaith U | Home" />
+        <meta
+          name="description"
+          content="Advaith U is a Web Developer, Community Evangelist, and tech speaker. He has prior experience working as a Google DSC Lead and Microsoft Student Ambassador."
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://advaith.co/" />
+        <meta property="og:title" content="Advaith U | Home" />
+        <meta
+          property="og:description"
+          content="Advaith U is a Web Developer, Community Evangelist, and tech speaker. He has prior experience working as a Google DSC Lead and Microsoft Student Ambassador."
+        />
+        <meta property="og:image" content="/advaith.png" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://advaith.co/" />
+        <meta property="twitter:title" content="Advaith U | Home" />
+        <meta
+          property="twitter:description"
+          content="Advaith U is a Web Developer, Community Evangelist, and tech speaker. He has prior experience working as a Google DSC Lead and Microsoft Student Ambassador."
+        />
+        <meta property="twitter:image" content="/advaith.png" />
       </Head>
       <Layout>
         <Section grid>
